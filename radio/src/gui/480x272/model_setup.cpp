697c87a50cd91ae842001b600ea50db25783662c
Tim Gold <timothyagold@live.com>
2019-05-22 00:17:44 -0600

    Update model_setup.cpp
    (cherry picked from commit 5a980ca0eb23316eb4cf57b27fbd80019265723e)

--- radio/src/gui/480x272/model_setup.cpp
+++ radio/src/gui/480x272/model_setup.cpp
@@ -736,8 +736,10 @@ void ModelSetupPage::build(Window * window)
     grid.addWindow(new ModuleWindow(window, {0, grid.getWindowHeight(), LCD_W, 0}, EXTERNAL_MODULE));
   }
 
+  // Trainer Mode
 #if defined(TRAINERMODULE)
   {
+    grid.nextLine();
     new StaticText(window, grid.getLabelSlot(), STR_TRAINER);
     new Choice(window, grid.getFieldSlot(), STR_VTRAINERMODES, 0, 3, GET_DEFAULT(g_model.trainerMode),
       [=](int32_t newValue) {
