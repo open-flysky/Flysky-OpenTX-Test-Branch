e5d14b90ee86be0291d54e9f3a6874aa0868bec3
Jakub <jklimasz@gmail.com>
2019-09-04 22:53:57 +0200

    LCD driver improved (LCD on/off)

--- radio/src/targets/nv14/board.cpp
+++ radio/src/targets/nv14/board.cpp
@@ -238,6 +238,16 @@ void boardInit()
 #endif
 }
 
+void lcdOff() {
+  lcdOffFunction();
+  backlightEnable(0); /* just disable the backlight */
+}
+
+void lcdOn(){
+  lcdOnFunction();
+  backlightEnable(BACKLIGHT_LEVEL_MAX);
+}
+
 
 void boardOff()
 {
