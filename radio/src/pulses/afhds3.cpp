0f6b847068141edde57def740a01cc92b8bff33d
Jakub <jklimasz@gmail.com>
2020-03-26 23:22:51 +0100

    Settings synchronization fixed for AFHDS3

--- radio/src/pulses/afhds3.cpp
+++ radio/src/pulses/afhds3.cpp
@@ -178,11 +178,7 @@ void afhds3::parseData(uint8_t* rxBuffer, uint8_t rxBufferCount) {
         break;
       case COMMAND::MODULE_GET_CONFIG:
           std::memcpy((void*)cfg.buffer, &responseFrame->value, sizeof(cfg.buffer));
-          TRACE("AFHDS3 [MODULE_GET_CONFIG] telemetry %d", cfg.config.telemetry);
-          moduleData->afhds3.bindPower = cfg.config.bindPower;
-          moduleData->afhds3.runPower = cfg.config.runPower;
-          moduleData->afhds3.telemetry = cfg.config.telemetry;
-          moduleData->afhds3.setMode(cfg.config.pulseMode == PULSE_MODE::PWM, cfg.config.serialMode == SERIAL_MODE::SBUS);
+          TRACE("AFHDS3 [MODULE_GET_CONFIG] bpow %d rpow %d tele %d pwm/ppm %d ibus/sbus %d", cfg.config.bindPower, cfg.config.runPower, cfg.config.telemetry, cfg.config.pulseMode, cfg.config.serialMode);
           break;
       case COMMAND::MODULE_VERSION:
         std::memcpy((void*)&version, &responseFrame->value, sizeof(version));
@@ -206,6 +202,7 @@ void afhds3::parseData(uint8_t* rxBuffer, uint8_t rxBufferCount) {
             addToQueue(COMMAND::MODULE_GET_CONFIG, FRAME_TYPE::REQUEST_GET_DATA);
             addToQueue(COMMAND::MODULE_STATE, FRAME_TYPE::REQUEST_GET_DATA);
           }
+          requestedModuleMode = MODULE_MODE_UNKNOWN;
         }
         break;
       case COMMAND::MODULE_SET_CONFIG:
@@ -321,7 +318,10 @@ void afhds3::setupPulses() {
   //TRACE("%d state %d repeatCount %d", (int)operationState, this->data->state, repeatCount);
   if(operationState == State::AWAITING_RESPONSE) {
     if(repeatCount++ < 5) return; //re-send
-    else reset(false);
+    else {
+        TRACE("AFHDS3 [NO RESP]");
+        reset(false);
+    }
   }
   else if(operationState == State::UNKNOWN){
     data->state = ModuleState::STATE_NOT_READY;
@@ -329,7 +329,7 @@ void afhds3::setupPulses() {
   data->ptr = data->pulses;
   repeatCount = 0;
   if (data->state == ModuleState::STATE_NOT_READY) {
-    TRACE("IS ready");
+    TRACE("AFHDS3 [GET MODULE READY]");
     putFrame(COMMAND::MODULE_READY, FRAME_TYPE::REQUEST_GET_DATA);
     return;
   }
@@ -355,48 +355,46 @@ void afhds3::setupPulses() {
     delete r;
     return;
   }
-
-  //config should be loaded already
-  if(syncSettings()) return;
-
+  
   static uint32_t commandIndex = 0;
   static const COMMAND commandsIDLE[] = {
       COMMAND::MODULE_STATE,
       COMMAND::MODULE_POWER_STATUS,
       COMMAND::MODULE_GET_CONFIG
   };
-  if(commandIndex == sizeof(commandsIDLE)) commandIndex = 0;
 
-  switch(data->state) {
-    case ModuleState::STATE_READY:
-        reset();
-        break;
-    case ModuleState::STATE_HW_ERROR:
-    case ModuleState::STATE_BINDING:
-    case ModuleState::STATE_UPDATING_RX:
-    case ModuleState::STATE_UPDATING_WAIT:
-    case ModuleState::STATE_UPDATING_MOD:
-    case ModuleState::STATE_HW_TEST:
-        if(idleCount++ % 128 == 0) putFrame(commandsIDLE[commandIndex++], FRAME_TYPE::REQUEST_GET_DATA);
-        break;
-    case ModuleState::STATE_SYNC_RUNNING:
-    case ModuleState::STATE_SYNC_DONE:
-       if((::failsafeCounter[EXTERNAL_MODULE]--) == 0) { //tbd 2.4 used better access to module index
-         failsafeCounter[EXTERNAL_MODULE] = 250;
-         TRACE("AFHDS FAILSAFE");
-         uint8_t failSafe[3+MAX_CHANNELS*2] = { 0x11, 0x60 };
-         uint8_t channels = setFailSafe((int16_t*)(failSafe + 3)); //M0 has problem with such alignment
-         failSafe[2] = channels *2;
-         putFrame(COMMAND::SEND_COMMAND, FRAME_TYPE::REQUEST_SET_EXPECT_DATA, failSafe, 3 + channels*2);
-         //trace("AFHDS3 [AFHDS3 SET FAILSAFE] data");
-       }
-       else{
-         if(idleCount++ % 512 == 0) {
-           putFrame(commandsIDLE[commandIndex++], FRAME_TYPE::REQUEST_GET_DATA);
-         }
-         else sendChannelsData();
-       }
-      break;
+  if(data->state == ModuleState::STATE_READY)
+  {
+     reset();
+     return;
+  }
+  bool sync = data->state == ModuleState::STATE_SYNC_RUNNING || data->state == ModuleState::STATE_SYNC_DONE;
+
+  if(idleCount-- == 0) {
+    idleCount = 250;
+    uint32_t max = sizeof(commandsIDLE);
+    //if we are binding we can not store settings
+    //so requesting settings will result in overriding user input
+    if(data->state == ModuleState::STATE_BINDING) {
+      max -= 1;
+    }
+    if(commandIndex == max) commandIndex = 0;
+    putFrame(commandsIDLE[commandIndex++], FRAME_TYPE::REQUEST_GET_DATA);
+  }
+  else if(sync) {
+    //config should be loaded already
+    if(syncSettings()) return;
+    if((::failsafeCounter[EXTERNAL_MODULE]--) == 0) { //tbd 2.4 used better access to module index
+     failsafeCounter[EXTERNAL_MODULE] = 250;
+     TRACE("AFHDS FAILSAFE");
+     uint8_t failSafe[3+MAX_CHANNELS*2] = { 0x11, 0x60 };
+     uint8_t channels = setFailSafe((int16_t*)(failSafe + 3)); //M0 has problem with such alignment
+     failSafe[2] = channels *2;
+     putFrame(COMMAND::SEND_COMMAND, FRAME_TYPE::REQUEST_SET_EXPECT_DATA, failSafe, 3 + channels*2);
+    }
+    else {
+      sendChannelsData();
+    }
   }
 }
 
