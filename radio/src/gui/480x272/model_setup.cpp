e9e48a09e6d074556e0d2a2baf0058422a5723d4
qba667 <jklimasz@gmail.com>
2020-10-21 18:30:50 +0200

    LCD fixed for ST7796S, defaul module data fixed.

--- radio/src/gui/480x272/model_setup.cpp
+++ radio/src/gui/480x272/model_setup.cpp
@@ -33,6 +33,16 @@ void resetModuleSettings(uint8_t module)
   if (isModulePPM(module)) {
     SET_DEFAULT_PPM_FRAME_LENGTH(EXTERNAL_MODULE);
   }
+#if defined(AFHDS2)
+  else if (isModuleFlysky(module)) {
+    g_model.moduleData[module].romData.setDefault();
+  }
+#endif
+#if defined(AFHDS3)
+  else if (isModuleAFHDS3(module)) {
+    g_model.moduleData[module].afhds3.setDefault();
+  }
+#endif
   setModuleFlag(module, MODULE_MODE_RESET_SETTINGS);
   setModuleFlag(module, MODULE_MODE_NORMAL);
 }
